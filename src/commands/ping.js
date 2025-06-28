b146e91eb1624c5b6793a37b4a505805dc076ad1
boolean44-repos <serenitypalmer10@gmail.com>
2025-06-28 00:29:33 -0400

    added ping

---
+++ src/commands/ping.js
@@ -0,0 +1,26 @@
+const { EmbedBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Sends bot ping")
+    .addBooleanOption(option =>
+        option.setName("private")
+        .setDescription("Can others see this message?")
+        .setRequired(true)
+    ),
+
+    run: async({interaction, client, handler}) => {
+        const hide = interaction.options.getBoolean("private")
+
+        const latency = Math.round(interaction.client.ws.ping);
+        const embed = new EmbedBuilder()
+            .setTitle('Latency')
+            .setDescription(`${latency}ms`)
+            .setColor(0x3498db); // Blue color
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
+       
