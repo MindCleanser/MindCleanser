179dd610ca1e1dd8c6c933e0af7e1b72f7c0e6e1
boolean44-repos <serenitypalmer10@gmail.com>
2025-07-08 08:27:14 -0400

    added nami

--- src/commands/nami.js
+++ src/commands/nami.js
@@ -1,12 +1,12 @@
 const { EmbedBuilder, ButtonBuilder } = require("@discordjs/builders");
 const { ActionRowBuilder } = require("@discordjs/builders");
 const { SlashCommandBuilder, ButtonStyle, MessageFlags } = require("discord.js");
-const { transLLWeb, transLLnum, transLLresources, transLLdonate, transLLthumb } =require("../../config.json");
+const { namiweb, namidonate, namiedu, namithumb } =require("../../config.json");
 const { ButtonKit } = require("commandkit");
     module.exports = {
     data: new SlashCommandBuilder()
-    .setName("trans-lifeline")
-    .setDescription("Displays info on Trans Lifeline")
+    .setName("nami")
+    .setDescription("Displays info on NAMI")
     .addBooleanOption(option =>
         option.setName("private")
         .setDescription("Can others see this message?")
@@ -16,20 +16,20 @@ const { ButtonKit } = require("commandkit");
     run: async ({interaction, client, handler}) => {
         const hide = interaction.options.getBoolean("private")
       const button = new ButtonBuilder()
-      .setLabel("Trans Lifeline")
+      .setLabel("NAMI")
       .setStyle(ButtonStyle.Link)
       
-      .setURL(transLLWeb)
+      .setURL(namiweb)
 
       const button2 = new ButtonBuilder()
-      .setLabel("Trans Lifeline")
+      .setLabel("NAMI educational resources")
       .setStyle(ButtonStyle.Link)
-      .setURL(transLLresources)
+      .setURL(namiedu)
 
       const button3 = new ButtonBuilder()
       .setLabel("Donate")
       .setStyle(ButtonStyle.Link)
-      .setURL(transLLdonate)
+      .setURL(namidonate)
       
       const row = new ActionRowBuilder()
       .addComponents(button, button2, button3)
@@ -37,13 +37,9 @@ const { ButtonKit } = require("commandkit");
      
 
      const embed = new EmbedBuilder()
-      .setTitle("Trans Lifeline")
-      .setDescription("The Trans Lifeline is an organization that provides information solely for the trans community. These include a helpline and a bunch of trans-related resources")
-      .setThumbnail(transLLthumb)
-      .addFields(
-        {name:"Contact information", value:`You can connect to a counsler by calling ${transLLnum}`},
-        {name: "Operating hours", value:"PST: 10AM-6PM MON-FRI\n EST: 1PM-9PM MON-FRI", inline:true}
-      )
+      .setTitle("NAMI")
+      .setDescription("NAMI is a large collection of mental health resources on a variety of different topics including mens mental health resources")
+      .setThumbnail(namithumb)
       
     if (hide == false) {
            const reply = interaction.reply({
