e267cad8c88165914cc5f52b6b2a7d284cb91221
boolean44-repos <serenitypalmer10@gmail.com>
2025-07-26 12:27:14 -0400

    updated status and readme

---
+++ CONTRIBUTING.md
@@ -0,0 +1,11 @@
+
+/*__Contributing
+
+If you would like to add to this project, please follow these guidelines:
+
+1. Don't use ANY ai
+2. have a basic understanding of [discord.js](https://discordjs.guide/#before-you-begin) as well as [CommandKit](https://commandkit.dev/)
+3. Add your name to contributors.json ny putting your name after the most recent \n and end it with a \n
+4. Include your discord username in the pull request (I will want to be testing your code on your test bot first)
+
+
--- src/sharding-manager.js
+++ src/sharding-manager.js
@@ -1,14 +1,14 @@
 import { ShardingManager } from 'discord.js';
 import { join } from 'node:path';
-
+import { Logger } from 'commandkit';
 process.loadEnvFile('./.env');
 
 const manager = new ShardingManager(join(import.meta.dirname, 'index.js'), {
   token: process.env.DISCORD_TOKEN,
-  totalShards: 2,
+  totalShards: 'auto',
   mode: 'worker',
 });
 
-manager.on('shardCreate', (shard) => console.log(`Launched shard ${shard.id}`));
+manager.on('shardCreate', (shard) => Logger.info(`Launched shard ${shard.id}`));
 
 await manager.spawn();
