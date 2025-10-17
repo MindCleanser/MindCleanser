ea9a46685922cfe0063804b2a56c8b4f43ebdc37
boolean44-repos <serenitypalmer10@gmail.com>
2025-10-17 17:26:48 -0400

    fixed hosting

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import { Client } from 'discord.js';
-
+import { spawn } from 'child_process';
 import { AutoPoster } from 'topgg-autoposter'
 import { Logger } from 'commandkit';
+
 
 process.loadEnvFile('./.env');
 
@@ -15,4 +16,5 @@ ap.on('posted', () => {
   Logger.info('Posted stats to Top.gg!')
 })
 
+spawn('npx', ['commandkit', 'dev'], { stdio: 'inherit', shell: true })
 export default client;
